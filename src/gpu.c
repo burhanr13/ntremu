653d5c4cc29bd43b2e0e472cb674cca93f0c6840
burhanr13 <burhan.rajgara@gmail.com>
2024-01-22 19:28:51 -0500

    gxfifo

--- src/gpu.c
+++ src/gpu.c
@@ -1,5 +1,75 @@
 #include "gpu.h"
 
+#include "io.h"
+#include "nds.h"
+
+const int cmd_parms[8][16] = {{0},
+                              {1, 0, 1, 1, 1, 0, 16, 12, 16, 12, 9, 3, 3},
+                              {1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1},
+                              {1, 1, 1, 1, 32},
+                              {1, 0},
+                              {1},
+                              {1},
+                              {3, 2, 1}};
+
 void gxfifo_write(GPU* gpu, u32 command) {
-    
+    if (gpu->params_pending) {
+        gpu->param_fifo[gpu->param_fifosize++] = command;
+        gpu->params_pending--;
+        gpu->master->io9.gxstat.gxfifo_size++;
+    } else {
+        while (command) {
+            u8 cmd = command;
+            u8 h = cmd >> 4;
+            u8 l = cmd & 0xf;
+            int nparms = 0;
+            if (h < 8) nparms = cmd_parms[h][l];
+            gpu->cmd_fifo[gpu->cmd_fifosize++] = cmd;
+            gpu->params_pending += nparms;
+            if (!nparms) gpu->master->io9.gxstat.gxfifo_size++;
+
+            command >>= 8;
+        }
+    }
+
+    if (!gpu->params_pending) {
+        while (gpu->cmd_fifosize) {
+            gxcmd_execute(gpu);
+        }
+    }
+
+    gpu->master->io9.gxstat.gxfifo_empty =
+        gpu->master->io9.gxstat.gxfifo_size ? 0 : 1;
+    gpu->master->io9.gxstat.gxfifo_half =
+        (gpu->master->io9.gxstat.gxfifo_size < 128) ? 1 : 0;
+    gpu->master->io9.ifl.gxfifo = (gpu->master->io9.gxstat.irq_empty &&
+                                   gpu->master->io9.gxstat.gxfifo_empty) ||
+                                  (gpu->master->io9.gxstat.irq_half &&
+                                   gpu->master->io9.gxstat.gxfifo_half);
+}
+
+void gxcmd_execute(GPU* gpu) {
+    u8 cmd = gpu->cmd_fifo[0];
+    switch (cmd) {
+        default:
+            break;
+    }
+
+    u8 h = cmd >> 4;
+    u8 l = cmd & 0xf;
+    int nparms = 0;
+    if (h < 8) nparms = cmd_parms[h][l];
+    gpu->cmd_fifosize--;
+    gpu->param_fifosize -= nparms;
+    for (int i = 0; i < gpu->cmd_fifosize; i++) {
+        gpu->cmd_fifo[i] = gpu->cmd_fifo[i + 1];
+    }
+    if (nparms) {
+        for (int i = 0; i < gpu->param_fifosize; i++) {
+            gpu->param_fifo[i] = gpu->param_fifo[i + nparms];
+        }
+        gpu->master->io9.gxstat.gxfifo_size -= nparms;
+    } else {
+        gpu->master->io9.gxstat.gxfifo_size--;
+    }
 }
--- src/gpu.h
+++ src/gpu.h
@@ -1,19 +1,48 @@
 #ifndef GPU_H
 #define GPU_H
 
+#include "ppu.h"
 #include "types.h"
+
+typedef struct {
+    s16 p[3];
+} vec3;
+
+typedef struct {
+    s32 p[4][4];
+} mat4;
 
 typedef struct _NDS NDS;
 
 typedef struct {
     NDS* master;
 
+    u16 screen[NDS_SCREEN_H][NDS_SCREEN_W];
+
     u8 cmd_fifo[256];
     u32 param_fifo[256];
     u8 cmd_fifosize;
     u8 param_fifosize;
+    u8 params_pending;
+
+    mat4 cur_projmtx;
+    mat4 proj_mtxstk[1];
+    u8 projstk_size;
+
+    mat4 cur_posmtx;
+    mat4 pos_mtxstk[32];
+    u8 posstk_size;
+
+    mat4 cur_vecmtx;
+    mat4 vec_mtxstk[32];
+    u8 vecstk_size;
+
+    vec3 vertexram[6144];
+    u16 n_verts;
+
 } GPU;
 
 void gxfifo_write(GPU* gpu, u32 command);
+void gxcmd_execute(GPU* gpu);
 
 #endif
