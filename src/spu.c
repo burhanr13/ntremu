841042f7b8ce0782c48f1893717ab8e1003cc28b
Burhan Rajgara <burhan.rajgara@gmail.com>
2024-02-04 01:12:06 -0500

    fixed audio

--- src/spu.c
+++ src/spu.c
@@ -4,14 +4,21 @@
 #include "io.h"
 #include "nds.h"
 
-float adpcm_table[89];
+float adpcm_table[89] = {
+    7,     8,     9,     10,    11,    12,    13,    14,    16,    17,
+    19,    21,    23,    25,    28,    31,    34,    37,    41,    45,
+    50,    55,    60,    66,    73,    80,    88,    97,    107,   118,
+    130,   143,   157,   173,   190,   209,   230,   253,   279,   307,
+    337,   371,   408,   449,   494,   544,   598,   658,   724,   796,
+    876,   963,   1060,  1166,  1282,  1411,  1552,  1707,  1878,  2066,
+    2272,  2499,  2749,  3024,  3327,  3660,  4026,  4428,  4871,  5358,
+    5894,  6484,  7132,  7845,  8630,  9493,  10442, 11487, 12635, 13899,
+    15289, 16818, 18500, 20350, 22385, 24623, 27086, 29794, 32767};
 const int adpcm_ind_table[8] = {-1, -1, -1, -1, 2, 4, 6, 8};
 
 void generate_adpcm_table() {
-    float x = 0x000776d2 / (float) (u32) (1 << 31);
     for (int i = 0; i < 89; i++) {
-        adpcm_table[i] = x;
-        x += x / 10;
+        adpcm_table[i] /= 0x8000;
     }
 }
 
@@ -51,6 +58,8 @@ void spu_reload_channel(SPU* spu, int i) {
             } else {
                 cur_sample += diff;
             }
+            if (cur_sample < -1) cur_sample = -1;
+            if (cur_sample > 1) cur_sample = 1;
             spu->adpcm_idx[i] += adpcm_ind_table[data & 7];
             if (spu->adpcm_idx[i] < 0) spu->adpcm_idx[i] = 0;
             if (spu->adpcm_idx[i] > 88) spu->adpcm_idx[i] = 88;
